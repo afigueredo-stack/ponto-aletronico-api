5ad71f24cc25acf59ea41500d38f87d101dd8945
Axel Figueredo <axelfigueredo07@gmail.com>
2021-01-03 20:14:38 -0300

    Projeto Ponto Eletroinico - Curso Spring Boot Restful Com Java 8

---
+++ src/test/java/com/afigueredo/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.afigueredo.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+////@RunWith(SpringRunner.class) --- Não necessário no Spring 2 --- Não necessário no Spring 2
+@SpringBootTest
+@ActiveProfiles("test")
+public class PontoInteligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
